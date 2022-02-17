0f977f1984dc8bfa0d2d8f1ca98fcf262159fcae
Marianne Xavier <97704587+xaviermarianne@users.noreply.github.com>
2022-02-16 22:10:57 -0300

    mapeando dados

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp()); 
+
+class  ExpensesApp extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     home:  MyHomePage()
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction (
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction (
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ]; 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar (
+        title: Text('Despesas Pessoai'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            child: Card(
+              color: Colors.blue,
+              child: Text('Gráfico'),
+              elevation: 5,
+            ),
+          ),
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+              child: Row (
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      tr.value.toString()
+                    ),
+                  ),
+                  Column(
+                    children: <Widget> [
+                      Text(tr.title),
+                      Text(tr.date.toString()), 
+                    ],
+                  )
+                ],
+              ),
+              );
+            }).toList(),
+          )
+        ],
+      ),
+    );
+  }
+}
