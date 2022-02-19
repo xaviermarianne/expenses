af72e3a1bf0f133a685e899a0f638e77abb94e9c
Marianne Xavier <97704587+xaviermarianne@users.noreply.github.com>
2022-02-18 22:22:01 -0300

    usando textfield

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
-
+import 'package:intl/intl.dart';
 main() => runApp(ExpensesApp()); 
 
 class  ExpensesApp extends StatelessWidget {
@@ -15,6 +15,10 @@ class  ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+
+  final titleController = TextEditingController(); 
+  final valueController = TextEditingController(); 
+
   final _transactions = [
     Transaction (
       id: 't1',
@@ -37,7 +41,6 @@ class MyHomePage extends StatelessWidget {
         title: Text('Despesas Pessoai'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
@@ -65,7 +68,7 @@ class MyHomePage extends StatelessWidget {
                     ),
                     padding: EdgeInsets.all(10),
                     child: Text(
-                      tr.value.toString(),
+                      'R\$ ${tr.value.toStringAsFixed(2)}',
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 20,
@@ -84,7 +87,7 @@ class MyHomePage extends StatelessWidget {
                         ),
                         ),
                       Text(
-                        tr.date.toString(), 
+                        DateFormat('dMMMy').format(tr.date), 
                         style: TextStyle(
                           color: Colors.grey,
                         ),
@@ -95,6 +98,41 @@ class MyHomePage extends StatelessWidget {
               ),
               );
             }).toList(),
+          ),
+          Card(
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    controller: titleController,
+                    decoration: InputDecoration(
+                     labelText: 'Título', 
+                    ),
+                  ),
+                  TextField(
+                    controller: valueController,
+                    decoration: InputDecoration(
+                      labelText: 'Valor (R\$)', 
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      FlatButton(
+                        child: Text('Nova Transação'),
+                        textColor: Colors.purple, 
+                        onPressed: () {
+                          print(titleController.text);
+                          print(valueController.text); 
+                        },
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
           )
         ],
       ),
